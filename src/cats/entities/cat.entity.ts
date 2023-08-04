10d7d87b6b05958fd6a3b1365bc83b060cfdfea7
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2023-08-04 18:54:07 -0300

    Dia 1.

---
+++ src/cats/entities/cat.entity.ts
@@ -0,0 +1,22 @@
+import { Breed } from 'src/breeds/entities/breed.entity';
+import { Column, DeleteDateColumn, Entity, ManyToOne } from 'typeorm';
+
+@Entity()
+export class Cat {
+  @Column({ primary: true, generated: true })
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @ManyToOne(() => Breed, (breed) => breed.id, {
+    eager: true, // para que traiga las raza al hacer un findOne
+  })
+  breed: Breed;
+}
